cd4b1f0a11d503c13fae00ed0027a71c2eb2258a
Ahmed Sghaier <a7mado008@gmail.com>
2026-06-24 01:44:31 +0200

    Refactor metrics controller registry

--- server/air_pollution/api/metrics/controller.py
+++ server/air_pollution/api/metrics/controller.py
@@ -1,116 +1,93 @@
-import falcon
-from dateutil.parser import parse
-from bson.json_util import dumps
 from datetime import datetime, timedelta
 
-from air_pollution.common.metric_evaluators import MeanAbsoluteErrorEvaluator, MeanAbsolutePercentageErrorEvaluator, \
-    MaximumPercentageErrorEvaluator, RootMeanSquaredErrorEvaluator, MeanSquaredErrorEvaluator, \
-    SymmetricMeanAbsolutePercentageErrorEvaluator
+import falcon
+from bson.json_util import dumps
+from dateutil.parser import parse
+
+from air_pollution.common.metric_evaluators import (
+    MaximumPercentageErrorEvaluator,
+    MeanAbsoluteErrorEvaluator,
+    MeanAbsolutePercentageErrorEvaluator,
+    MeanSquaredErrorEvaluator,
+    RootMeanSquaredErrorEvaluator,
+    SymmetricMeanAbsolutePercentageErrorEvaluator,
+)
+
+
+DEFAULT_LOOKBACK_DAYS = 3000
+
+METRIC_DEFINITIONS = [
+    {
+        'key': 'mean-absolute-error',
+        'name': 'Mean absolute error',
+        'short': 'MAE',
+        'evaluator': MeanAbsoluteErrorEvaluator,
+    },
+    {
+        'key': 'mean-absolute-percentage-error',
+        'name': 'Mean absolute percentage error',
+        'short': 'MAPE',
+        'evaluator': MeanAbsolutePercentageErrorEvaluator,
+    },
+    {
+        'key': 'symmetric-mean-absolute-percentage-error',
+        'name': 'Symmetric mean absolute percentage error',
+        'short': 'SMAPE',
+        'evaluator': SymmetricMeanAbsolutePercentageErrorEvaluator,
+    },
+    {
+        'key': 'maximum-percentage-error',
+        'name': 'Maximum percentage error',
+        'short': 'MPE',
+        'evaluator': MaximumPercentageErrorEvaluator,
+    },
+    {
+        'key': 'mean-squared-error',
+        'name': 'Mean squared error',
+        'short': 'MSE',
+        'evaluator': MeanSquaredErrorEvaluator,
+    },
+    {
+        'key': 'root-mean-squared-error',
+        'name': 'Root mean squared error',
+        'short': 'RMSE',
+        'evaluator': RootMeanSquaredErrorEvaluator,
+    },
+]
+
+METRICS_BY_KEY = {metric['key']: metric for metric in METRIC_DEFINITIONS}
 
 
 class MetricsController(object):
     def on_get(self, req, res):
         res.body = dumps([
             {
-                'key': 'mean-absolute-error',
-                'name': 'Mean absolute error',
-                'short': 'MAE'
-            },
-            {
-                'key': 'mean-absolute-percentage-error',
-                'name': 'Mean absolute percentage error',
-                'short': 'MAPE'
-            },
-            {
-                'key': 'symmetric-mean-absolute-percentage-error',
-                'name': 'Symmetric mean absolute percentage error',
-                'short': 'SMAPE'
-            },
-            {
-                'key': 'maximum-percentage-error',
-                'name': 'Maximum percentage error',
-                'short': 'MPE'
-            },
-            {
-                'key': 'mean-squared-error',
-                'name': 'Mean squared error',
-                'short': 'MSE'
-            },
-            {
-                'key': 'root-mean-squared-error',
-                'name': 'Root mean squared error',
-                'short': 'RMSE'
-            },
+                'key': metric['key'],
+                'name': metric['name'],
+                'short': metric['short'],
+            }
+            for metric in METRIC_DEFINITIONS
         ], ensure_ascii=False)
         res.status = falcon.HTTP_200
 
 
 class StationMetricsController(object):
     def on_get(self, req, res, predictor, station_name):
-        if 'from' in req.params:
-            from_date = parse(req.params['from'])
-        else:
-            from_date = datetime.now() - timedelta(days=3000)
-
-        if 'to' in req.params:
-            to_date = parse(req.params['to'])
-        else:
-            to_date = datetime.now()
+        from_date, to_date = parse_metric_date_range(req)
 
         body = []
-        mean_absolute_error = MeanAbsoluteErrorEvaluator().calculate_from_db(station_id=station_name,
-                                                                             from_date=from_date,
-                                                                             to_date=to_date, predictor=predictor)
-        mean_absolute_percentage_error = MeanAbsolutePercentageErrorEvaluator().calculate_from_db(
-            station_id=station_name,
-            from_date=from_date,
-            to_date=to_date,
-            predictor=predictor)
-        maximum_percentage_error = MaximumPercentageErrorEvaluator().calculate_from_db(station_id=station_name,
-                                                                                       from_date=from_date,
-                                                                                       to_date=to_date,
-                                                                                       predictor=predictor)
-        mean_squared_error = MeanSquaredErrorEvaluator().calculate_from_db(station_id=station_name, from_date=from_date,
-                                                                           to_date=to_date, predictor=predictor)
-        root_mean_squared_error = RootMeanSquaredErrorEvaluator().calculate_from_db(station_id=station_name,
-                                                                                    from_date=from_date,
-                                                                                    to_date=to_date,
-                                                                                    predictor=predictor)
-        smape = SymmetricMeanAbsolutePercentageErrorEvaluator().calculate_from_db(station_id=station_name,
-                                                                                  from_date=from_date,
-                                                                                  to_date=to_date,
-                                                                                  predictor=predictor)
-
-        body.append({
-            'name': 'mean-absolute-error',
-            'type': 'mean-absolute-error',
-            'values': mean_absolute_error
-        })
-        body.append({
-            'name': 'mean-absolute-percentage-error',
-            'type': 'mean-absolute-percentage-error',
-            'values': mean_absolute_percentage_error
-        })
-        body.append({
-            'name': 'symmetric-mean-absolute-percentage-error',
-            'type': 'symmetric-mean-absolute-percentage-error',
-            'values': smape
-        })
-        body.append({
-            'name': 'maximum-percentage-error',
-            'type': 'maximum-percentage-error',
-            'values': maximum_percentage_error
-        })
-        body.append({
-            'name': 'mean-squared-error',
-            'type': 'mean-squared-error',
-            'values': mean_squared_error
-        })
-        body.append({
-            'name': 'root-mean-squared-error',
-            'type': 'root-mean-squared-error',
-            'values': root_mean_squared_error
-        })
+        for metric in METRIC_DEFINITIONS:
+            values = metric['evaluator']().calculate_from_db(
+                station_id=station_name,
+                from_date=from_date,
+                to_date=to_date,
+                predictor=predictor,
+            )
+            body.append({
+                'name': metric['key'],
+                'type': metric['key'],
+                'values': values,
+            })
 
         res.body = dumps(body, ensure_ascii=False)
         res.status = falcon.HTTP_200
@@ -118,36 +95,25 @@ class StationMetricsController(object):
 
 class MetricController(object):
     def on_get(self, req, res, predictor, station_name, metric_name):
-        if 'from' in req.params:
-            from_date = parse(req.params['from'])
-        else:
-            from_date = datetime.now() - timedelta(days=3000)
-
-        if 'to' in req.params:
-            to_date = parse(req.params['to'])
-        else:
-            to_date = datetime.now()
-
-        if metric_name == 'mean-absolute-error':
-            body = MeanAbsoluteErrorEvaluator().calculate(station_id=station_name, from_date=from_date, to_date=to_date,
-                                                          predictor=predictor)
-        elif metric_name == 'mean-absolute-percentage-error':
-            body = MeanAbsoluteErrorEvaluator().calculate(station_id=station_name, from_date=from_date, to_date=to_date,
-                                                          predictor=predictor)
-        elif metric_name == 'maximum-percentage-error':
-            body = MaximumPercentageErrorEvaluator().calculate(station_id=station_name, from_date=from_date,
-                                                               to_date=to_date,
-                                                               predictor=predictor)
-        elif metric_name == 'root-mean-squared-error':
-            body = RootMeanSquaredErrorEvaluator().calculate(station_id=station_name, from_date=from_date,
-                                                             to_date=to_date, predictor=predictor)
-        elif metric_name == 'mean-squared-error':
-            body = MeanSquaredErrorEvaluator().calculate(station_id=station_name, from_date=from_date,
-                                                         to_date=to_date, predictor=predictor)
-        else:
+        metric = METRICS_BY_KEY.get(metric_name)
+        if metric is None:
             res.body = dumps({'error': metric_name + ' not known!'})
             res.status = falcon.HTTP_400
             return
 
+        from_date, to_date = parse_metric_date_range(req)
+        body = metric['evaluator']().calculate_from_db(
+            station_id=station_name,
+            from_date=from_date,
+            to_date=to_date,
+            predictor=predictor,
+        )
+
         res.body = dumps(body, ensure_ascii=False)
         res.status = falcon.HTTP_200
+
+
+def parse_metric_date_range(req):
+    from_date = parse(req.params['from']) if 'from' in req.params else datetime.now() - timedelta(days=DEFAULT_LOOKBACK_DAYS)
+    to_date = parse(req.params['to']) if 'to' in req.params else datetime.now()
+    return from_date, to_date
